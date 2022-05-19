caa1bcc082cf61a062ae620e2ed838e13b28e16b
teo-mtn <teo.martin@epitech.eu>
2022-05-19 16:34:22 +0200

    Adding PUT, DELETE et GET for a team with error handling in case the team isn't found

--- workee_backend/src/Client/Controller/Team/TeamController.php
+++ workee_backend/src/Client/Controller/Team/TeamController.php
@@ -3,6 +3,7 @@
 namespace App\Client\Controller\Team;
 
 use App\Core\Entity\Team;
+use App\Core\Entity\Company;
 use App\Core\Services\JsonResponseService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,7 +24,7 @@ class TeamController extends AbstractController
     }
 
     /**
-     * @Route("/api/team", name="login", methods={"POST"})
+     * @Route("/api/team", name="createTeam", methods={"POST"})
      */
     public function createTeam(Request $request): Response
     {
@@ -52,9 +53,67 @@ class TeamController extends AbstractController
         $response = array();
 
         foreach ($teams as $team) {
-            $response = TeamViewModel::createByTeam($team);
+            array_push($response, TeamViewModel::createByTeam($team));
         }
 
+        return new JsonResponse($response);
+    }
+
+    /**
+    * @Route("/api/team/{id}", name="removeTeam", methods={"DELETE"})
+    */
+    public function removeTeam(Request $request, $id): Response
+    {
+        $team = $this->teamRepository->findOneById($id);
+       
+        if (!$team) {
+            $response = new JsonResponseService();
+            $response = $response->errorJsonResponse("No Team Found with the selected ID", 404);
+            return $response;          
+        }
+
+        $this->teamRepository->remove($team);
+
+        return new Response('Team successfully erased !');
+    }
+
+    /**
+    * @Route("/api/team/{id}", name="editTeam", methods={"put"})
+    */
+    public function editTeam(Request $request, $id): Response
+    {
+        $teamData = json_decode($request->getContent(), true);
+
+        $team = $this->teamRepository->findOneById($id);
+
+        if (!$team) {
+            $response = new JsonResponseService();
+            $response = $response->errorJsonResponse("No Team Found with the selected ID", 404);
+            return $response;
+        }
+
+        $team->setTeamName($teamData["teamName"]);
+
+        $this->teamRepository->add($team);
+
+        $response = TeamViewModel::createByTeam($team);
+        return new JsonResponse($response);
+    }
+    
+    /**
+    * @Route("/api/team/{id}", name="getTeamById", methods={"get"})
+    */
+    public function getTeam(Request $request, $id): Response
+    {
+        $team = $this->teamRepository->findOneById($id);
+
+        if (!$team) {
+            $response = new JsonResponseService();
+            $response = $response->errorJsonResponse("No Team Found with the selected ID", 404);
+            return $response;
+        }
+
+        $response = TeamViewModel::createByTeam($team);
         return new JsonResponse($response);
     }
 }
