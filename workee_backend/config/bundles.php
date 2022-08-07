378a55bbbf0ab7ceb435fb4cdb63ec1885313a6e
valentin.lyon <valentin.lyon@travaux.com>
2022-08-07 15:34:48 +0200

    routes RPGD CGU

--- workee_backend/config/bundles.php
+++ workee_backend/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     SpecShaper\EncryptBundle\SpecShaperEncryptBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
