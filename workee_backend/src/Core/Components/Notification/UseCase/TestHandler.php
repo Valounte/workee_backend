fd56de36dac1048ed85f80d934969960210796b7
valentin <valentin.lyon@epitech.eu>
2022-09-15 15:42:38 +0200

    bas les couilles

--- workee_backend/src/Core/Components/Notification/UseCase/TestHandler.php
+++ workee_backend/src/Core/Components/Notification/UseCase/TestHandler.php
@@ -2,19 +2,41 @@
 
 namespace App\Core\Components\Notification\UseCase;
 
-use App\Core\Components\Notification\Repository\NotificationRepositoryInterface;
 use Symfony\Component\Mercure\Update;
 use Symfony\Component\Mercure\HubInterface;
+use App\Infrastructure\Token\Services\TokenService;
+use Symfony\Component\Messenger\MessageBusInterface;
+use App\Core\Components\Notification\Entity\Notification;
+use App\Core\Components\Notification\UseCase\TestCommand;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use App\Core\Components\Notification\UseCase\NotificationCommand;
-use App\Infrastructure\Token\Services\TokenService;
+use App\Core\Components\Notification\Repository\NotificationRepositoryInterface;
 
 final class TestHandler implements MessageHandlerInterface
 {
+    public function __construct(
+        private NotificationRepositoryInterface $notificationRepository,
+        private HubInterface $hub,
+        private TokenService $tokenService,
+        private MessageBusInterface $messageBus,
+        private string $mercureHubUrl,
+    ) {
+    }
+
     public function __invoke(TestCommand $command): void
     {
-        $message = $command->getmessage();
+        $myCommand = $command;
+        
+        $update = new Update(
+            $this->mercureHubUrl . '/notification' . '/' . "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiY29tcGFueSI6MX0.eOe0BAe5N9aCCdwB7ETFrrOpQaHSWJUsejayELA-SmU",
+            json_encode([
+                "alertLevel" => "important",
+                "recepteurId" => 1,
+                "message" => "zouzoubizouz"
+            ])
+        );
 
-        // Do something with $message
+
+        $this->hub->publish($update);
     }
 }
