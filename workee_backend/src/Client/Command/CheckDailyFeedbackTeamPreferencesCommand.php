bedd282fd95cc48e61988366b453b8b9fba76b5d
teo.mtn13 <teo.martin@epitech.eu>
2022-12-09 13:34:57 +0100

    Creating a console command to check if there are Daily Feedbacks to send

---
+++ workee_backend/src/Client/Command/CheckDailyFeedbackTeamPreferencesCommand.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Client\Command;
+
+use App\Core\Components\Feedback\Entity\DailyFeedbackTeamPreferences;
+use App\Core\Components\Feedback\Repository\DailyFeedbackTeamPreferencesRepositoryInterface;
+use App\Core\Components\Feedback\UseCase\TeamNeedsToSendFeedbackEvent;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsCommand(
+    name: 'app:checkDailyFeedbackPreferences',
+    description: 'Checks if there is Daily Feedbacks to send in the next minute in order to put them in queue',
+    hidden: false,
+)]
+class CheckDailyFeedbackTeamPreferencesCommand extends Command {
+    protected static $defaultDescription = 'Checks if there is Daily Feedbacks to send in the next minute in order to put them in queue';
+
+    function __construct(
+       private DailyFeedbackTeamPreferencesRepositoryInterface $dailyFeedbackTeamPreferencesRepository,
+       private MessageBusInterface $messageBus,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            // the command help shown when running the command with the "--help" option
+            ->setHelp('This command allows you to Checks if there is Daily feedbacks to send in the next minute in order to put them in queue...')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $dailyFeedbacksPreferences = $this->dailyFeedbackTeamPreferencesRepository->findPreferencesInNextMinute();
+
+        dd($dailyFeedbacksPreferences);
+
+        if ($dailyFeedbacksPreferences === []) {
+            return Command::SUCCESS;
+        }
+
+
+        foreach ($dailyFeedbacksPreferences as $value) {
+            $event = new TeamNeedsToSendFeedbackEvent($value->getId(), $value->getSendingTime());
+            dump("coucou");
+            $this->messageBus->dispatch($event);
+        }
+        return Command::SUCCESS;
+    }
+}
