23917da41f4677c6d690449d16ab6d127aec40d4
teo.mtn13 <teo.martin@epitech.eu>
2022-12-09 14:36:27 +0100

    Implementing the notification sending in Team Feedback Listener

--- workee_backend/src/Core/Components/Feedback/UseCase/TeamNeedsToSendFeedbackListener.php
+++ workee_backend/src/Core/Components/Feedback/UseCase/TeamNeedsToSendFeedbackListener.php
@@ -1,14 +1,36 @@
 <?php
 
 namespace App\Core\Components\Feedback\UseCase;
+use App\Infrastructure\Token\Services\TokenService;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use App\Core\Components\Feedback\UseCase\TeamNeedsToSendFeedbackEvent;
+use App\Core\Components\Team\Repository\TeamRepositoryInterface;
+use App\Core\Components\User\Repository\UserTeamRepositoryInterface;
 
 final class TeamNeedsToSendFeedbackListener implements MessageHandlerInterface {
-    public function __construct() {}
+    public function __construct(
+        private TokenService $tokenService,
+        private UserTeamRepositoryInterface $userTeamRepository,
+        private HubInterface $hub,
+        private TeamRepositoryInterface $teamRepository,
+        private string $mercureHubUrl,
+    ) {
+    }
 
     public function __invoke(TeamNeedsToSendFeedbackEvent $teamNeedsToSendFeedback)
     {
+        $team = $teamNeedsToSendFeedback->getTeam();
+        $users = $this->userTeamRepository->findUsersByTeamId($team);
 
+        foreach ($users as $user) {
+            $jwt = $this->tokenService->createLoginToken($user);
+            $update = new Update(
+                $this->mercureHubUrl . '/feedback' . '/' . $jwt,
+            );
+
+            $this->hub->publish($update);
+        }
     }
 }
