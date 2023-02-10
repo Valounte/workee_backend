8805ada4a8b45fa4e05bff18edeacfa3ba32ca30
Valentin LYON <71253694+Valounte@users.noreply.github.com>
2023-02-10 10:35:59 +0100

    dispatch mercure notif if metric preference updated (#81)
    Co-authored-by: valentin.lyon <valentin.lyon@travaux.com>

---
+++ workee_backend/src/Core/Components/EnvironmentMetrics/UseCase/EnvironmentMetricsPreferences/EnvironmentMetricPreferenceUpdatedListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Core\Components\EnvironmentMetrics\UseCase\EnvironmentMetricsPreferences;
+
+use Symfony\Component\Mercure\Update;
+use Symfony\Component\Mercure\HubInterface;
+use App\Infrastructure\Token\Services\TokenService;
+use App\Core\Components\User\Repository\UserRepositoryInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use App\Core\Components\EnvironmentMetrics\UseCase\EnvironmentMetricsPreferences\EnvironmentMetricPreferenceUpdatedEvent;
+
+final class EnvironmentMetricPreferenceUpdatedListener implements MessageHandlerInterface
+{
+    public function __construct(
+        private UserRepositoryInterface $userRepository,
+        private HubInterface $hub,
+        private string $mercureHubUrl,
+        private TokenService $tokenService,
+    ) {
+    }
+
+    public function __invoke(EnvironmentMetricPreferenceUpdatedEvent $event): void
+    {
+        $user = $this->userRepository->findUserById($event->getUserId());
+        $jwt = $this->tokenService->createLoginToken($user);
+
+        $update = new Update(
+            $this->mercureHubUrl . '/metrics-preferences' . '/' . $jwt,
+            json_encode([
+                'metricType' => $event->getMetricType(),
+                'isDesactivated' => $event->getIsDesactivated(),
+            ])
+        );
+        $this->hub->publish($update);
+    }
+}
