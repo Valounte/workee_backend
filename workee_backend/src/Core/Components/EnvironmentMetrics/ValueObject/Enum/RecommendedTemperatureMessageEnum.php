d1bb7ba3e8d5709335fded79354f20a92d45160b
valentin.lyon <valentin.lyon@travaux.com>
2023-02-10 17:05:14 +0100

    Message recommended

--- workee_backend/src/Core/Components/EnvironmentMetrics/ValueObject/Enum/RecommendedTemperatureMessageEnum.php
+++ workee_backend/src/Core/Components/EnvironmentMetrics/ValueObject/Enum/RecommendedTemperatureMessageEnum.php
@@ -4,9 +4,9 @@ namespace App\Core\Components\EnvironmentMetrics\ValueObject\Enum;
 
 enum RecommendedTemperatureMessageEnum: string
 {
-    case WARNING_TEMPERATURE_TOO_LOW =  'La température est basse, laissez un peu rentrer la chaleur ou chauffez légèrement la pièce.';
-    case WARNING_TEMPERATURE_TOO_HIGH =  'La température est élevée, refroidissez légèrement la pièce.';
-    case ALERT_TEMPERATURE_TOO_LOW = 'La température est très basse, laissez rentrer la chaleur ou chauffez la pièce.';
-    case ALERT_TEMPERATURE_TOO_HIGH = 'La température est très élevée, refroidissez la pièce.';
-    case CONFORM_TEMPERATURE = 'La température est idéale';
+    case WARNING_TEMPERATURE_TOO_LOW =  'La température est basse. Laissez un peu rentrer la chaleur ou chauffez légèrement la pièce.';
+    case WARNING_TEMPERATURE_TOO_HIGH =  'La température est élevée. Refroidissez légèrement la pièce.';
+    case ALERT_TEMPERATURE_TOO_LOW = 'La température est très basse. Laissez rentrer la chaleur ou chauffez la pièce.';
+    case ALERT_TEMPERATURE_TOO_HIGH = 'La température est très élevée. Refroidissez la pièce.';
+    case CONFORM_TEMPERATURE = 'La température est idéale.';
 }
